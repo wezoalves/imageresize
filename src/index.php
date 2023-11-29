b14783a91eed0a1b53ce8996c352a271e967e0f9
Weslley Alves <weslley@wezo.com.br>
2023-11-28 22:24:59 -0300

    add try catch on generate new image

--- src/index.php
+++ src/index.php
@@ -5,14 +5,13 @@ include("../vendor/autoload.php");
 use Grafika\Grafika;
 use ImgOnthefly\NameFile;
 
-$url = $_GET['url']??null;
-$width = $_GET['w']??null;
-$height = $_GET['h']??null;
-$crop = $_GET['crop']??"smart";
+$url = $_GET['url'] ?? null;
+$width = $_GET['w'] ?? null;
+$height = $_GET['h'] ?? null;
+$crop = $_GET['crop'] ?? ($_GET['clipping'] ?? "smart");
 
 
-if(!$url){
-  header('HTTP/1.0 404 Not Found', true, 404);
+if (!$url) {
   header('Content-Type: image/gif');
   echo base64_decode('R0lGODlhAQABAJAAAP8AAAAAACH5BAUQAAAALAAAAAABAAEAAAICBAEAOw==');
   exit();
@@ -22,13 +21,13 @@ if(!$url){
 $file = new NameFile($url, $width, $height, $crop);
 
 // if image original not exists
-if(!$file->getFile(1)){
+if (!$file->getFile(1)) {
 
   // storage image
   $saved = @file_put_contents(dirname(__FILE__) . "/image/{$file->getFolder()}/{$file->getOriginal()}", file_get_contents($file->url));
 
   // on error, return placeholder
-  if(!$saved){
+  if (!$saved) {
     header('Content-Type: image/gif');
     echo base64_decode('R0lGODlhAQABAJAAAP8AAAAAACH5BAUQAAAALAAAAAABAAEAAAICBAEAOw==');
     exit();
@@ -37,13 +36,22 @@ if(!$file->getFile(1)){
 }
 
 // if image optimized not exists
-if(!$file->getFile(2)){
+if (!$file->getFile(2)) {
 
-  // create a image optimized
-  $editor = Grafika::createEditor();
-  $editor->open( $image, $file->getFile(1) );
-  $editor->resizeFill( $image, $width, $height, $crop );
-  $editor->save( $image, "image/{$file->getFolder()}/{$file->getOptimized()}");
+  try {
+    // create a image optimized
+    $editor = Grafika::createEditor();
+    $editor->open($image, $file->getFile(1));
+    $editor->resizeFill($image, $width, $height, $crop);
+    $editor->save($image, "image/{$file->getFolder()}/{$file->getOptimized()}");
+
+  } catch (\Throwable $th) {
+    header('Content-Type: image/gif');
+    echo base64_decode('R0lGODlhAQABAJAAAP8AAAAAACH5BAUQAAAALAAAAAABAAEAAAICBAEAOw==');
+    exit();
+  }
+
+
 }
 
 // if optimized image not created, return original image
